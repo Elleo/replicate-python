5aa9b3fa3794cefb6a11e5c3a31dd24f7bbd6641
Zeke Sikelianos <zeke@sikelianos.com>
2022-05-20 09:37:02 -0700

    set __version__
    Signed-off-by: Zeke Sikelianos <zeke@sikelianos.com>

--- replicate/__init__.py
+++ replicate/__init__.py
@@ -3,3 +3,7 @@ from .client import Client
 default_client = Client()
 models = default_client.models
 predictions = default_client.predictions
+
+package_version = None
+exec(open('replicate/package_version.py').read())
+__version__ = package_version
